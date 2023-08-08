b690e8c2acccc5df046caa78fe01f12735ef3d3e
Jayaprakash Jaisankar <116724255+JAYAPRAKASHJAISANKAR@users.noreply.github.com>
2023-08-08 16:36:57 +0530

    CLOUD DB

--- src/main/java/registration/dao/UserDAO.java
+++ src/main/java/registration/dao/UserDAO.java
@@ -6,12 +6,33 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import registration.model.User;
 
 public class UserDAO {
 
 	// Connect to the database
 	public Connection getConnection() throws SQLException {
+		String DB_URL;
+		String DB_USER;
+		String DB_PASSWORD;
+
+		if (System.getenv("CI") != null) {
+			DB_URL = System.getenv("DB_URL");
+			DB_USER = System.getenv("DB_USER");
+			DB_PASSWORD = System.getenv("DB_PASSWORD");
+		} else {
+			Dotenv env = Dotenv.load();
+			DB_URL = env.get("DB_URL");
+			DB_USER = env.get("DB_USER");
+			DB_PASSWORD = env.get("DB_PASSWORD");
+		}
+		
+		
+		/*
+		 * DB_URL,DB_USER,DB_PASSWORD
+		 */		
+		
 		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/backend", "root", "123456");
 		return connection;
 	}
@@ -45,6 +66,7 @@ public class UserDAO {
 
 	// Add new user to DB - Register
 	public boolean register(User user) throws SQLException {
+		
 		// Get Connection
 		Connection connection = getConnection();
 
